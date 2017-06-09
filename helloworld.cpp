147d59837a651308da67686503187bf39e030048
gitfong <799618118@qq.com>
2017-06-09 13:31:22 +0800

    Create helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int getMax(int a, int b);
+
 int main()
 {  
-  cout<<"hello world, welcome to github."<<endl;
+  cout<<"hello world, welcome to github."<<endl;  
   return 0;
+}
+
+int getMax(int a, int b)
+{
+  return a>b?a:b;
 }
